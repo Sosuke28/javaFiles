799a1829f4176613092e42458c5496cdfb6c5198
guecoerwin11-coder <guecoerwin11@gmail.com>
2026-02-16 08:23:27 +0800

    updated

--- jsClosure.js
+++ jsClosure.js
@@ -14,26 +14,3 @@
 // let counts = outer()
 // counts()
 // counts()
-
-function bankAccount(initialBalance){
-    let balance = initialBalance
-
-    return{
-        deposit(amount){
-            balance += amount
-            return `you deposited ${amount} and your new balance is ${balance}`;
-        },
-        getBal(){
-            return balance;
-        },
-        withdraw(amount){
-            balance -= amount;
-            return `you withdraw ${amount}, and your balance now is ${balance}`;
-        }
-    }
-}
-
-const myBalance = bankAccount(1000);
-console.log(myBalance.withdraw(350))
-console.log(myBalance.deposit(450))
-console.log(myBalance.getBal())
